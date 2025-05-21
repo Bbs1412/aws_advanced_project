6ef77e5180213826ce7f443298a10de33443b326
Bhushan Songire <bhushanbsongire@gmail.com>
2025-05-21 13:16:43 +0530

    Refactor database ops to port from SQLite to MySQL using pymysql

--- sq_db.py
+++ sq_db.py
@@ -1,9 +1,55 @@
-# Sqlite code to create the db and all:
+# MySQL code to create the db and all:
 
 import os
 import json
-import sqlite3
+import pymysql
 from datetime import datetime
+from logger import create_log
+from dotenv import load_dotenv
+
+
+# -----------------------------------------------------------------------------
+# Decide database and define connection:
+# -----------------------------------------------------------------------------
+
+load_dotenv()
+
+DB_MODE = os.environ.get("DB_MODE")
+create_log("DB", f"Using Database Mode: {DB_MODE}")
+
+# Determine DB configuration based on mode
+try:
+    # Local MySQL Database:
+    if DB_MODE == 'local':
+        DB_CONFIG = {
+            "host": os.environ.get("LOCAL_DB_HOST"),
+            "user": os.environ.get("LOCAL_DB_USER"),
+            "password": os.environ.get("LOCAL_DB_PASSWORD"),
+            "db": os.environ.get("LOCAL_DB_DB"),
+            "charset": os.environ.get("LOCAL_DB_CHARSET"),
+        }
+
+    # Remote MySQL Database (RDS):
+    elif DB_MODE == 'remote':
+        DB_CONFIG = {
+            "host": os.environ.get("DB_HOST"),
+            "user": os.environ.get("DB_USER"),
+            "password": os.environ.get("DB_PASSWORD"),
+            "db": os.environ.get("DB_DB"),
+            "charset": os.environ.get("DB_CHARSET"),
+        }
+
+    def get_db_connection():
+        if DB_MODE in ['local', 'remote']:
+            return pymysql.connect(**DB_CONFIG)         # type: ignore
+        else:
+            raise Exception("DB_MODE not set correctly in .env file.")
+
+except Exception as e:
+    print(f"Database connection error: {e}")
+    create_log("DB", f"Database connection error: {e}")
+    # exit(1)
+
 
 # -------------------------------------------------------------------------------------
 # Database:
@@ -11,32 +57,16 @@ from datetime import datetime
 
 # Clear entire database:
 def clear_db():
-    # backup the db into db_backup folder:
-    if not os.path.exists('db_backup'):
-        os.mkdir('db_backup')
-
-    os.system(
-        f'copy lite.db db_backup\lite{datetime.now().strftime("_%Y-%m-%d_%H-%M")}.db')
-
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        DROP TABLE IF EXISTS LOCATIONS
-        """
-    )
-    cur.execute(
-        """
-        DROP TABLE IF EXISTS MAPS
-        """
-    )
-    cur.execute(
-        """
-        DROP TABLE IF EXISTS LOCATION_DETAILS
-        """
-    )
-    conn.commit()
-    conn.close()
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute("DROP TABLE IF EXISTS LOCATIONS")
+                cur.execute("DROP TABLE IF EXISTS MAPS")
+                cur.execute("DROP TABLE IF EXISTS LOCATION_DETAILS")
+                conn.commit()
+        create_log("DB", "Database cleared successfully.")
+    except Exception as e:
+        create_log("DB", f"Error clearing database: {e}")
 
 
 # -------------------------------------------------------------------------------------
@@ -45,62 +75,72 @@ def clear_db():
 
 # Create the Locations table to hold the basic data:
 def create_locations():
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS LOCATIONS (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            location TEXT,
-            image TEXT, image2 TEXT,
-            image3 TEXT, image4 TEXT
-        )
-        """
-    )
-    conn.commit()
-    conn.close()
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS LOCATIONS (
+                        id INT PRIMARY KEY,
+                        name TEXT,
+                        location TEXT,
+                        image TEXT,
+                        image2 TEXT,
+                        image3 TEXT,
+                        image4 TEXT
+                    )
+                    """
+                )
+            conn.commit()
+    except Exception as e:
+        create_log("DB", f"Error creating LOCATIONS table: {e}")
 
 
 # Create the Maps table to hold the map data for the location:
 def create_maps():
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS MAPS (
-            id INTEGER PRIMARY KEY,
-            location_id INTEGER,
-            map_iframe TEXT,
-            latitude REAL,
-            longitude REAL,
-            FOREIGN KEY (location_id) REFERENCES LOCATIONS(id)
-        )
-        """
-    )
-    conn.commit()
-    conn.close()
+    create_log("DB", "Creating the table MAPS.")
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS MAPS (
+                        id INT PRIMARY KEY,
+                        location_id INT,
+                        map_iframe TEXT,
+                        latitude FLOAT,
+                        longitude FLOAT,
+                        FOREIGN KEY (location_id) REFERENCES LOCATIONS(id)
+                    )
+                    """
+                )
+            conn.commit()
+    except Exception as e:
+        create_log("DB", f"Error creating table MAPS: {e}")
 
 
 # Details table to hold the details of the location for web page:
 def create_location_details():
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS LOCATION_DETAILS (
-            id INTEGER PRIMARY KEY,
-            location_id INTEGER,
-            description_intro TEXT,
-            description_history TEXT,
-            description_highlights TEXT,
-            description_tips TEXT,
-            FOREIGN KEY (location_id) REFERENCES LOCATIONS(id)
-        )
-        """
-    )
-    conn.commit()
-    conn.close()
+    create_log("DB", "Creating the table LOCATION_DETAILS.")
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS LOCATION_DETAILS (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        location_id INT,
+                        description_intro TEXT,
+                        description_history TEXT,
+                        description_highlights TEXT,
+                        description_tips TEXT,
+                        FOREIGN KEY (location_id) REFERENCES LOCATIONS(id) 
+                    )
+                    """
+                )
+            conn.commit()
+    except Exception as e:
+        create_log("DB", f"Error creating table LOCATION_DETAILS: {e}")
 
 
 # -------------------------------------------------------------------------------------
@@ -109,48 +149,46 @@ def create_location_details():
 
 # Fn to insert data into Locations table:
 def insert_location(id, name, location, image, image2, image3, image4):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO LOCATIONS (id, name, location, image, image2, image3, image4)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """,
-        (id, name, location, image, image2, image3, image4)
-    )
-    conn.commit()
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO LOCATIONS (id, name, location, image, image2, image3, image4)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """,
+                (id, name, location, image, image2, image3, image4)
+            )
+            conn.commit()
 
 
 # Fn to insert data into Maps table:
 def insert_map(location_id, map_iframe, latitude, longitude):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO MAPS (location_id, map_iframe, latitude, longitude)
-        VALUES (?, ?, ?, ?)
-        """,
-        (location_id, map_iframe, latitude, longitude)
-    )
-    conn.commit()
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO MAPS (location_id, map_iframe, latitude, longitude)
+                VALUES (%s, %s, %s, %s)
+                """,
+                (location_id, map_iframe, latitude, longitude)
+            )
+            conn.commit()
 
 
 # Fn to insert data into Location Details table:
 def insert_location_details(location_id, description_intro, description_history, description_highlights, description_tips):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        INSERT INTO LOCATION_DETAILS (location_id, description_intro, description_history, description_highlights, description_tips)
-        VALUES (?, ?, ?, ?, ?)
-        """,
-        (location_id, description_intro, description_history,
-         description_highlights, description_tips)
-    )
-    conn.commit()
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO LOCATION_DETAILS (location_id, description_intro, description_history, description_highlights, description_tips)
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                (location_id, description_intro, description_history,
+                 description_highlights, description_tips)
+            )
+            conn.commit()
+
 
 # -------------------------------------------------------------------------------------
 # Helper Functions:
@@ -171,98 +209,64 @@ def get_iframe_code(src, width=350, height=300):
 
 # Fn to call from server which returns location based on id (for specific location page):
 def get_location_data(id, output_format='json'):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT * FROM LOCATIONS WHERE id = ?
-        """,
-        (id,)
-    )
-    location = cur.fetchone()
-    headers = [description[0] for description in cur.description]
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT * FROM LOCATIONS WHERE id = %s
+                """,
+                (id,)
+            )
+            location = cur.fetchone()
+            headers = [desc[0] for desc in cur.description]
 
-    resp = {}
-    if location:
-        resp = dict(zip(headers, location))
-
-    if output_format == 'dict':
-        return resp
-    else:
-        return json.dumps(resp, indent=5)
+    resp = dict(zip(headers, location)) if location else {}
+    return json.dumps(resp, indent=5) if output_format == 'json' else resp
 
 
 # Get map data of location based on location_id:
 def get_map_data(location_id, output_format='json'):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT * FROM MAPS WHERE location_id = ?
-        """,
-        (location_id,)
-    )
-    location_map = cur.fetchone()
-    headers = [description[0] for description in cur.description]
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT * FROM MAPS WHERE location_id = %s
+                """,
+                (location_id,)
+            )
+            location_map = cur.fetchone()
+            headers = [desc[0] for desc in cur.description]
 
-    resp = {}
-    if location_map:
-        resp = dict(zip(headers, location_map))
-
-    if output_format == 'json':
-        return json.dumps(resp, indent=5)
-    else:
-        return resp
+    resp = dict(zip(headers, location_map)) if location_map else {}
+    return json.dumps(resp, indent=5) if output_format == 'json' else resp
 
 
 # Get all locations: (meant to be called from the client side js)
 def get_all_locations_data(output_format='json'):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT * FROM LOCATIONS
-        """
-    )
-    locations = cur.fetchall()
-    headers = [description[0] for description in cur.description]
-    conn.close()
-    locations_list = []
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM LOCATIONS")
+            locations = cur.fetchall()
+            headers = [desc[0] for desc in cur.description]
 
-    for location in locations:
-        location_dict = dict(zip(headers, location))
-        locations_list.append(location_dict)
-
-    if output_format == 'json':
-        return json.dumps(locations_list, indent=5)
-    else:
-        return locations_list
+    locations_list = [dict(zip(headers, row)) for row in locations]
+    return json.dumps(locations_list, indent=5) if output_format == 'json' else locations_list
 
 
 # Get location details (text data) to render the page for a specific location:
 def get_place_details(place_id, output_format='json'):
-    # only return location_id, description_intro, description_history, description_highlights, description_tips (json format by default):
-    conn = sqlite3.connect('lite.db')
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT location_id, description_intro, description_history, description_highlights, description_tips
-        FROM LOCATION_DETAILS
-        WHERE location_id = ?
-        """,
-        (place_id,)
-    )
-    location_details = cur.fetchone()
-    headers = [description[0] for description in cur.description]
-    conn.close()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT location_id, description_intro, description_history, description_highlights, description_tips
+                FROM LOCATION_DETAILS
+                WHERE location_id = %s
+                """,
+                (place_id,)
+            )
+            location_details = cur.fetchone()
+            headers = [desc[0] for desc in cur.description]
 
-    resp = {}
-    if location_details:
-        resp = dict(zip(headers, location_details))
-
-    if output_format == 'json':
-        return json.dumps(resp, indent=5)
-    else:
-        return resp
+    resp = dict(zip(headers, location_details)) if location_details else {}
+    return json.dumps(resp, indent=5) if output_format == 'json' else resp
