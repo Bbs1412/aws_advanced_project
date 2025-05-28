39f84caac3cf1c45b1e4c9511f5ec1f0dd21757e
Bhushan Songire <bhushanbsongire@gmail.com>
2025-05-29 01:05:37 +0530

    Refactor app.py to use environment variables for image directory and S3 base URL; enhance logging for API requests

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
 import os
 import json
 from pprint import pprint
-from flask import Flask, render_template, jsonify, send_from_directory, abort
+from logger import create_log
+from flask import Flask, render_template, jsonify, send_from_directory, abort, request
 from sq_db import get_all_locations_data, get_place_details, get_location_data, get_map_data
 
 app = Flask(__name__)
 
-IMAGE_DIRECTORY = os.path.join(app.root_path, 'static/images')
-S3_BASE_URL = "Enter S3 bucket URL here: https://<bucket_name>.s3.<region>.amazonaws.com"
+IMAGE_DIRECTORY = os.environ.get("IMAGE_DIRECTORY")
+S3_BASE_URL = os.environ.get("S3_BASE_URL")
 
 
 @app.route('/test')
 def index():
+    create_log("App", f"{request.remote_addr} hit the test api.")
     return jsonify("server is live"), 200
 
 
-@app.route('/')
+@app.route("/")
 def main():
-    return render_template('index.html'), 200
+    create_log("App", f"{request.remote_addr} visited the main page.")
+    return render_template("index.html"), 200
 
 
-@app.route('/get_all_locations')
+@app.route("/get_all_locations")
 def get_all_locations():
+    create_log("DB", f"{request.remote_addr} requested all locations data.")
     # print(get_all_locations_data(output_format='json'))
-    return get_all_locations_data(output_format='json'), 200
+    return get_all_locations_data(output_format="json"), 200
 
 
-@app.route('/get_img/<img_name>')
+@app.route("/get_img/<img_name>")
 def get_image(img_name):
+    create_log("Img", f"{request.remote_addr} requested image {img_name}.")
+
     try:
         return send_from_directory(IMAGE_DIRECTORY, img_name), 200
     except FileNotFoundError:
         abort(404)
 
 
-@app.route('/place/<int:place_id>')
+@app.route("/place/<int:place_id>")
 def place_detail(place_id):
-    place_data = get_location_data(place_id, output_format='dict')
-    place_details = get_place_details(place_id, output_format='dict')
-    place_map = get_map_data(place_id, output_format='dict')
+    create_log("DB", f"{request.remote_addr} requested place {place_id} data.")
+
+    place_data = get_location_data(place_id, output_format="dict")
+    place_details = get_place_details(place_id, output_format="dict")
+    place_map = get_map_data(place_id, output_format="dict")
 
     final_data = place_data.copy()
     for key, value in place_details.items():
@@ -48,13 +56,15 @@ def place_detail(place_id):
         final_data[key] = value
 
     final_data["s3_base_url"] = S3_BASE_URL
-    return render_template('place_detail.html', place=final_data), 200
+    return render_template("place_detail.html", place=final_data), 200
 
 
 @app.route('/get_s3_base_url')
 def get_s3_base_url():
+    create_log("Env", f"{request.remote_addr} requested 'S3_base_URL'.")
     return jsonify(S3_BASE_URL), 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    create_log("App", "Sever Started", pre_lines=1, post_lines=1)
+    app.run(debug=True, host="0.0.0.0", port=5000)
