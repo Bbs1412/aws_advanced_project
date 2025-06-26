bb47068dc97ac6e1f3face4745ecfc43c01ab6a0
Bhushan Songire <bhushanbsongire@gmail.com>
2025-06-27 02:59:06 +0530

    Minor changes and complete v3 push! - .gitignore : add env and todo files - database_ops : adjust the map dimensions to ensure responsive design

--- database_ops.py
+++ database_ops.py
@@ -280,7 +280,7 @@ def insert_country(name, code, flag):
 # Helper Functions:
 # -------------------------------------------------------------------------------------
 
-def get_iframe_code(src, width=350, height=300):
+def get_iframe_code(src, width=330, height=300):
     defaults_just_saved = '''
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14994.081656486365!2d75.15761048715821!3d20.02863330000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb92609201a9eb%3A0xe0639ba6286a474!2sEllora%20Cave%20No.%2029%20The%20Dhumar%20Lena!5e0!3m2!1sen!2sin!4v1731098951522!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade"></iframe>
